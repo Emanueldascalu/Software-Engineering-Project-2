15ea718781236dc85a67edd3e26e49c94644e184
Emanueldascalu <emandascalu51@gmail.com>
2020-02-14 23:29:23 +0000

    Idea for displaying board

--- Board.java
+++ Board.java
@@ -1,11 +1,11 @@
-
+import java.util.*;
 public class Board 
 {
 	//Add enum with TL, DL, TW and DW or N
 	//State of the board at the beginning of the game.
 	//Abbreviations for Triple Word, Double Word, Triple Letter and Double Letter
 	//@@ = Middle Square
-	public String[][] board = 
+	private String[][] board = 
 		{
 				{"TW", "  ", "  ", "DL", "  ", "  ", "  ", "TW", "  ", "  ", "  ", "DL", "  ", "  ", "TW"},
 				{"  ", "DW", "  ", "  ", "  ", "TL", "  ", "  ", "  ", "TL", "  ", "  ", "  ", "DW", "  "},
@@ -24,14 +24,25 @@ public class Board
 				{"TW", "  ", "  ", "DL", "  ", "  ", "  ", "TW", "  ", "  ", "  ", "DL", "  ", "  ", "TW"}
 		};
 	
+	public void printLine() 
+	{
+		System.out.println("----------------------------------------------");
+	}
+	
 	public void displayBoard()
 	{
+		printLine();
 		for(int i = 0; i < 15; i++) 
 		{
+			System.out.print("|");
 			for(int j = 0; j < 15; j++) 
 			{
-				
+				System.out.print(board[i][j] + "|");
 			}
+			
+			System.out.println();
+			printLine();
 		}
+		
 	}
 }
---
+++ BoardTest.java
@@ -0,0 +1,9 @@
+
+public class BoardTest 
+{
+	public static void main(String[] args) 
+	{
+		Board b = new Board();
+		b.displayBoard();
+	}
+}
