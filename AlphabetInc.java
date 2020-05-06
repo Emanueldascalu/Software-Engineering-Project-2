9b9f654db9f22910c323ac3668a0e7c32538fb84
Emanueldascalu <emandascalu51@gmail.com>
2020-05-06 01:29:19 +0100

    Modularised the code a bit more

--- AlphabetInc.java
+++ AlphabetInc.java
@@ -78,8 +78,6 @@ public class AlphabetInc implements BotAPI {
     	if(turnCount >= 2) 
     	{
     		ArrayList<Word> words = new ArrayList();
-        	Word mostPointsWord = new Word(0, 0, true, " ");
-        	int mostPoints = 0;
         	
     		ArrayList<Character> lettersToBePermuted = new ArrayList();
     		for(int i = 0; i < lettersOnFrame.length(); i++) 
@@ -89,27 +87,19 @@ public class AlphabetInc implements BotAPI {
     		
 			ArrayList<String> allPossiblePermutations = new ArrayList();
 			
-			
 	    	if(board.isFirstPlay())
 	    	{
-	    		
 				int k_Of_N = 7;
 				int offset = 1;
 				permK_of_N(lettersToBePermuted, 0, k_Of_N, allPossiblePermutations);
 				
-
-				findFirstWord(dict, allPossiblePermutations, words, frame, offset, blankDesignations.toString());
-				
-
+				findWords1stTurn(offset, dict, allPossiblePermutations, blankDesignations.toString(), words, frame);
 				
 				while(words.isEmpty() && offset < 7 && k_Of_N > 1) 
 				{
 					permK_of_N(lettersToBePermuted, 0, k_Of_N - 1, allPossiblePermutations);
 					
-					
-					findFirstWord(dict, allPossiblePermutations, words, frame, offset + 1, blankDesignations.toString());
-					
-					
+					findWords1stTurn(offset + 1, dict, allPossiblePermutations, blankDesignations.toString(), words, frame);
 						
 					k_Of_N--;
 					offset++;
@@ -137,15 +127,13 @@ public class AlphabetInc implements BotAPI {
 				int offset = 6;
 				permK_of_N(lettersToBePermuted, 0, k_Of_N, allPossiblePermutations);
 	        	
-				findWords(dict, allPossiblePermutations, words, frame, offset, tileLocations, blankDesignations.toString());
-					
+				findWordsNthTurn(offset, dict, allPossiblePermutations, tileLocations, blankDesignations.toString(), words, frame, b);
 				
 	        	while(words.isEmpty() && offset > 0 && k_Of_N > 1) 
 				{
 					permK_of_N(lettersToBePermuted, 0, k_Of_N - 1, allPossiblePermutations);
 					
-					findWords(dict, allPossiblePermutations, words, frame, offset - 1, tileLocations, blankDesignations.toString());
-						
+					findWordsNthTurn(offset - 1, dict, allPossiblePermutations, tileLocations, blankDesignations.toString(), words, frame, b);	
 					
 					k_Of_N--;
 					offset--;
@@ -154,65 +142,50 @@ public class AlphabetInc implements BotAPI {
 	    	
 	    	if(words.isEmpty()) 
 			{
-	    		Random rand1 = new Random();
-	    		int numToBeExchanged = rand1.nextInt(lettersOnFrame.length());
-	    		StringBuilder str = new StringBuilder();
-	    		str.append("EXCHANGE ");
-	    		
-	    		ArrayList<Integer> al = new ArrayList();
-	    		al.add(0);
-	    		al.add(1);
-	    		al.add(2);
-	    		al.add(3);
-	    		al.add(4);
-	    		al.add(5);
-	    		al.add(6);
-	    		
-	    		for(int i = 0; i < numToBeExchanged; i++) 
+	    		if(lettersOnFrame.length() < 7) 
 	    		{
-	    			Random rand2 = new Random();
-	    			int index = rand2.nextInt(al.size());
-	    			str.append(lettersOnFrame.charAt(index));
-	    			al.remove(index);
+	    			command = "PASS";
 	    		}
 	    		
-				command = str.toString();
+	    		else 
+	    		{
+		    		Random rand1 = new Random();
+		    		
+		    		ArrayList<Character> frameAsCharList = new ArrayList<>();
+		    		for(int i = 0; i < lettersOnFrame.length(); i++) 
+		    		{
+		    			frameAsCharList.add(lettersOnFrame.charAt(i));
+		    		}
+		    		
+		    		StringBuilder str = new StringBuilder();
+		    		str.append("EXCHANGE ");
+		    		
+		    		int numToBeExchanged = rand1.nextInt(lettersOnFrame.length());
+		    		ArrayList<Character> charsToBeExchanged = new ArrayList();
+		    		Random rand2 = new Random();
+		    		
+		    		for(int i = 0; i < numToBeExchanged; i++) 
+		    		{
+		    			int frameIndex = rand2.nextInt(frameAsCharList.size());
+		    			charsToBeExchanged.add(frameAsCharList.get(frameIndex));
+		    			frameAsCharList.remove(frameIndex);
+		    			
+		    		}
+		    		
+		    		for(int i = 0; i < charsToBeExchanged.size(); i++) 
+		    		{
+		    			str.append(charsToBeExchanged.get(i).toString());
+		    			//charsToBeExchanged.remove(i);
+		    		}
+		    		
+					command = str.toString();
+				}
+	    		
 			}
 			
 			else 
-			{
-				for(int i = 0; i < words.size(); i++) 
-				{
-					if(wordScore(words.get(i)) > mostPoints) 
-					{
-						mostPointsWord = words.get(i);
-						mostPoints = wordScore(words.get(i));
-					}
-				}
-				
-				StringBuilder str = new StringBuilder();
-				char c = (char) ((int) 'A' + mostPointsWord.getFirstColumn());
-				int r = mostPointsWord.getFirstRow() + 1;
-				char across;
-				
-				if(mostPointsWord.isHorizontal())
-					across = 'A';
-				
-				else
-					across = 'D';
-				
-				String letters = mostPointsWord.getLetters();
-				
-				str.append(c);
-				str.append(r);
-				str.append(" " + across + " " + letters);
-				
-				if(blankDesignations.length() > 0) 
-				{
-					str.append(" " + blankDesignations.toString());
-				}
-				
-				command = str.toString();
+			{	
+				command = parseWord(words, blankDesignations.toString());
 			}
     	}
         
@@ -220,7 +193,46 @@ public class AlphabetInc implements BotAPI {
         return command;
     }
     
-    public void findFirstWord(Trie dict, ArrayList<String> allPossiblePermutations, ArrayList<Word> words, Frame frame, int offset, String blankDesignations) 
+    public String parseWord(ArrayList<Word> words, String blankDesignations) 
+    {
+    	Word mostPointsWord = new Word(0, 0, true, " ");
+    	int mostPoints = 0;
+    	for(int i = 0; i < words.size(); i++) 
+		{
+			if(wordScore(words.get(i)) > mostPoints) 
+			{
+				mostPointsWord = words.get(i);
+				mostPoints = wordScore(words.get(i));
+			}
+		}
+		
+		StringBuilder str = new StringBuilder();
+		char c = (char) ((int) 'A' + mostPointsWord.getFirstColumn());
+		int r = mostPointsWord.getFirstRow() + 1;
+		char direction;
+		
+		if(mostPointsWord.isHorizontal())
+			direction = 'A';
+		
+		else
+			direction = 'D';
+		
+		String letters = mostPointsWord.getLetters();
+		
+		str.append(c);
+		str.append(r);
+		str.append(" " + direction + " " + letters);
+		
+		if(blankDesignations.length() > 0) 
+		{
+			str.append(" " + blankDesignations);
+		}
+		
+		return str.toString();
+    }
+    
+    public void findWords1stTurn(int offset, Trie dict, ArrayList<String> allPossiblePermutations, 
+    		String blankDesignations, ArrayList<Word> words, Frame frame) 
     {
     	for(int i = 0; i < allPossiblePermutations.size(); i++) 
 		{
@@ -242,47 +254,23 @@ public class AlphabetInc implements BotAPI {
 			
 			if(dict.search(possibleLetters.toString())) 
 			{		
-				Random acrossOrDown = new Random();
-				if(acrossOrDown.nextInt(2) == 1)
+				Random rand = new Random();
+				int acrossOrDown = rand.nextInt(2);
+				if(acrossOrDown == 1) 
 				{
-					for(int r = offset; r <= 7; r++) 
-					{	
-						if(possibleWord.contains("_")) 
-						{
-							if(board.isLegalPlay(frame, new Word(r, 7, false, possibleWord, blankDesignations)))
-							words.add(new Word(r, 7, false, possibleWord, blankDesignations));
-						}
-						
-						else
-						{
-							if(board.isLegalPlay(frame, new Word(r, 7, false, possibleWord)))
-							words.add(new Word(r, 7, false, possibleWord));
-						}
-					}
+					addWords1stTurn(offset, true, possibleWord, blankDesignations.toString(), words, frame);
 				}
 				
-				else 
+				else if(acrossOrDown == 0)
 				{
-					for(int c = offset; c <= 7; c++) 
-					{
-						if(possibleWord.contains("_")) 
-						{
-							if(board.isLegalPlay(frame, new Word(7, c, true, possibleWord, blankDesignations)))
-							words.add(new Word(7, c, true, possibleWord, blankDesignations));
-						}
-						
-						else
-						{
-							if(board.isLegalPlay(frame, new Word(7, c, true, possibleWord)))
-							words.add(new Word(7, c, true, possibleWord));
-						}
-					}
+					addWords1stTurn(offset, false, possibleWord, blankDesignations.toString(), words, frame);
 				}
 			}
-		}
+		}	
     }
     
-    public void findWords(Trie dict, ArrayList<String> allPossiblePermutations, ArrayList<Word> words, Frame frame, int offset, ArrayList<Coordinates> tileLocations, String blankDesignations) 
+    public void findWordsNthTurn(int offset, Trie dict, ArrayList<String> allPossiblePermutations, 
+    		ArrayList<Coordinates> tileLocations, String blankDesignations, ArrayList<Word> words, Frame frame, Square[][] squares) 
     {
     	for(int i = 0; i < allPossiblePermutations.size(); i++) 
 		{
@@ -310,154 +298,124 @@ public class AlphabetInc implements BotAPI {
 					int row = coordinate.getRow();
 					int col = coordinate.getCol();
 					
-					for(int r = row - offset; r <= row; r++) 
-					{	
-						if(r >= 0 && r <= 14 && col >= 0 && col <= 14)
-						{	
-							if(possibleWord.contains("_")) 
-							{
-								if(board.isLegalPlay(frame, new Word(r, col, false, possibleWord, blankDesignations)))	
-								words.add(new Word(r, col, false, possibleWord, blankDesignations));
-							}
-							
-							else 
-							{
-								if(board.isLegalPlay(frame, new Word(r, col, false, possibleWord)))	
-								words.add(new Word(r, col, false, possibleWord));
-							}
-						}
-					}
-					
-					for(int c = col - offset; c <= col; c++) 
-					{
-						if(c >= 0 && c <= 14 && row >= 0 && row <= 14)
-						{	
-							if(possibleWord.contains("_")) 
-							{
-								if(board.isLegalPlay(frame, new Word(row, c, true, possibleWord, blankDesignations)))	
-								words.add(new Word(row, c, true, possibleWord, blankDesignations));
-							}
-							
-							else 
-							{
-								if(board.isLegalPlay(frame, new Word(row, c, true, possibleWord)))	
-								words.add(new Word(row, c, true, possibleWord));
-							}
-						}
-					}
-					
-					for(int r = row - offset; r <= row; r++) 
-					{	
-						if(r >= 0 && r <= 14 && col - 1 >= 0 && col - 1 <= 14)
-						{	
-							if(possibleWord.contains("_")) 
-							{
-								if(board.isLegalPlay(frame, new Word(r, col - 1, false, possibleWord, blankDesignations)))
-								words.add(new Word(r, col - 1, false, possibleWord, blankDesignations));
-							}
-							
-							else 
-							{
-								if(board.isLegalPlay(frame, new Word(r, col - 1, false, possibleWord)))
-								words.add(new Word(r, col - 1, false, possibleWord));
-							}
-						}
-					}
-					
-					for(int c = col - offset; c <= col; c++) 
-					{
-						if(c >= 0 && c <= 14 && row - 1 >= 0 && row - 1 <= 14) 
-						{
-							if(possibleWord.contains("_")) 
-							{
-								if(board.isLegalPlay(frame, new Word(row - 1, c, true, possibleWord, blankDesignations)))
-								words.add(new Word(row - 1, c, true, possibleWord, blankDesignations));
-							}
-							
-							else 
-							{
-								if(board.isLegalPlay(frame, new Word(row - 1, c, true, possibleWord)))
-								words.add(new Word(row - 1, c, true, possibleWord));
-							}
-						}
-					}
-					
-					for(int r = row - offset; r <= row; r++) 
-					{	
-						if(r >= 0 && r <= 14 && col + 1 >= 0 && col + 1 <= 14)
-						{
-							if(possibleWord.contains("_")) 
-							{
-								if(board.isLegalPlay(frame, new Word(r, col + 1, false, possibleWord, blankDesignations)))
-								words.add(new Word(r, col + 1, false, possibleWord, blankDesignations));
-							}
-							
-							else 
-							{
-								if(board.isLegalPlay(frame, new Word(r, col + 1, false, possibleWord)))
-								words.add(new Word(r, col + 1, false, possibleWord));
-							}
-						}
-					}
-					
-					for(int c = col - offset; c <= col; c++) 
-					{
-						if(c >= 0 && c <= 14 && row + 1 >= 0 && row + 1 <= 14) 
-						{
-							if(possibleWord.contains("_")) 
-							{
-								if(board.isLegalPlay(frame, new Word(row + 1, c, true, possibleWord, blankDesignations)))
-								words.add(new Word(row + 1, c, true, possibleWord, blankDesignations));
-							}
-							
-							else 
-							{
-								if(board.isLegalPlay(frame, new Word(row + 1, c, true, possibleWord)))
-								words.add(new Word(row + 1, c, true, possibleWord));
-							}
-						}
-					}
+					addWordsNthTurn(row, col, offset, true, possibleWord, blankDesignations, words, frame, squares);
+					addWordsNthTurn(row, col, offset, false, possibleWord, blankDesignations, words, frame, squares);
+					addWordsNthTurn(row - 1, col, offset, true, possibleWord, blankDesignations, words, frame, squares);
+					addWordsNthTurn(row, col - 1, offset, false, possibleWord, blankDesignations, words, frame, squares);
+					addWordsNthTurn(row + 1, col, offset, true, possibleWord, blankDesignations, words, frame, squares);
+					addWordsNthTurn(row, col + 1, offset, false, possibleWord, blankDesignations, words, frame, squares);
 				}
 			}
 		}
     }
     
+    public void addWords1stTurn(int offset, boolean isHorizontal, String possibleWord, String blankDesignations, 
+    		ArrayList<Word> words, Frame frame) 
+    {
+    	
+    	if(isHorizontal) 
+    	{
+    		for(int c = offset; c <= 7; c++) 
+    		{
+    			if(possibleWord.contains("_")) 
+    			{
+    				if(board.isLegalPlay(frame, new Word(7, c, true, possibleWord, blankDesignations)))
+    				words.add(new Word(7, c, true, possibleWord, blankDesignations));
+    			}
+    			
+    			else
+    			{
+    				if(board.isLegalPlay(frame, new Word(7, c, true, possibleWord)))
+    				words.add(new Word(7, c, true, possibleWord));
+    			}
+    		}
+    	}
+    	
+    	else 
+    	{
+			for(int r = offset; r <= 7; r++) 
+			{	
+				if(possibleWord.contains("_")) 
+				{
+					if(board.isLegalPlay(frame, new Word(r, 7, false, possibleWord, blankDesignations)))
+					words.add(new Word(r, 7, false, possibleWord, blankDesignations));
+				}
+				
+				else
+				{
+					if(board.isLegalPlay(frame, new Word(r, 7, false, possibleWord)))
+					words.add(new Word(r, 7, false, possibleWord));
+				}
+			}
+    	}
+	}
+    
+    public void addWordsNthTurn(int row, int column, int offset, boolean isHorizontal, String possibleWord, 
+    		String blankDesignations, ArrayList<Word> words, Frame frame, Square[][] squares) 
+    {
+    	if(isHorizontal) 
+    	{
+    		for(int c = column - offset; c <= column; c++) 
+    		{	
+    			if(c >= 0 && c <= 14 && row >= 0 && row <= 14)
+    			{	
+    				Word word;
+    				if(possibleWord.contains("_")) 
+    				{
+    					word = new Word(row, c, isHorizontal, possibleWord, blankDesignations);
+    				}
+    				
+    				else 
+    				{
+    					word = new Word(row, c, isHorizontal, possibleWord);
+    				}
+    				
+    				//ArrayList<Word> allAdditionalWords = getAllWords(word, 15, squares);
+					if(board.isLegalPlay(frame, word) /*&& dictionary.areWords(allAdditionalWords)*/)
+					words.add(word);
+    			}
+    		}
+    	}
+    	
+    	else 
+    	{ 
+	    	for(int r = row - offset; r <= row; r++) 
+			{	
+				if(r >= 0 && r <= 14 && column >= 0 && column <= 14)
+				{	
+					Word word;
+					if(possibleWord.contains("_")) 
+					{
+						word = new Word(r, column, isHorizontal, possibleWord, blankDesignations);
+					}
+					
+					else 
+					{
+						word = new Word(r, column, isHorizontal, possibleWord);
+					}
+					
+					//ArrayList<Word> allAdditionalWords = getAllWords(word, 15, squares);
+					if(board.isLegalPlay(frame, word) /*&& dictionary.areWords(allAdditionalWords)*/)	
+					words.add(word);
+				}
+			}
+    	}
+    }
+    
     public int wordScore(Word w) 
     {
     	int wordValue = 0;
-		int wordMultipler = 1;
+		int wordMultiplier = 1;
 		int letterValue;
 		int r = w.getFirstRow();
 		int c = w.getFirstColumn();
 		for (int i = 0; i < w.length(); i++) 
 		{
+			Square square = board.getSquareCopy(r, c);
 			Tile t = new Tile(w.getLetters().charAt(i));
 			letterValue = t.getValue();
-			
-			if(board.getSquareCopy(r, c).isDoubleLetter())
-			{
-				wordValue += (letterValue * 2);
-			}
-			
-			else if(board.getSquareCopy(r, c).isDoubleWord()) 
-			{
-				wordMultipler *= 2;  
-			}
-			
-			else if(board.getSquareCopy(r, c).isTripleLetter())
-			{
-				wordValue += (letterValue * 3);
-			}
-			
-			else if(board.getSquareCopy(r, c).isTripleWord())
-			{
-				wordMultipler *= 3;
-			}
-			
-			else 
-			{
-				wordValue = wordValue + letterValue;
-			}
+			wordMultiplier *= square.getWordMultiplier();
+			wordValue += (letterValue * square.getLetterMuliplier());
 			
 			if (w.isHorizontal()) 
 			{
@@ -470,7 +428,7 @@ public class AlphabetInc implements BotAPI {
 			}
 		}
 		
-		return wordValue * wordMultipler;
+		return wordValue * wordMultiplier;
     }
     
     public Trie createDictionary()
@@ -519,5 +477,70 @@ public class AlphabetInc implements BotAPI {
 			Collections.swap(n, i, j);
 		}
 	}
+	
+	/*public boolean isAdditionalWord(int r, int c, boolean isHorizontal, 
+			int BOARD_SIZE, Square[][] squares) 
+	{
+		if ((r >= 0 && r <= 14 && c >= 0 && c <= 14) && (isHorizontal &&
+				((r>0 && squares[r-1][c].isOccupied()) || (r<BOARD_SIZE-1 && squares[r+1][c].isOccupied()))) ||
+				(!isHorizontal &&
+				((c>0 && squares[r][c-1].isOccupied()) || (c<BOARD_SIZE-1 && squares[r][c+1].isOccupied()))) ) {
+			return true;
+		}
+		return false;
+	}
+
+	public Word getAdditionalWord(int mainWordRow, int mainWordCol, 
+			boolean mainWordIsHorizontal, int BOARD_SIZE, Square[][] squares) {
+		int firstRow = mainWordRow;
+		int firstCol = mainWordCol;
+		// search up or left for the first letter
+		while (firstRow >= 0 && firstCol >= 0 && squares[firstRow][firstCol].isOccupied()) {
+			if (mainWordIsHorizontal) {
+				firstRow--;
+			} else {
+				firstCol--;
+			}
+		}
+		// went too far
+		if (mainWordIsHorizontal) {
+			firstRow++;
+		} else {
+			firstCol++;
+		}
+		// collect the letters by moving down or right
+		String letters = "";
+		int r = firstRow;
+		int c = firstCol;
+		while (r<BOARD_SIZE && c<BOARD_SIZE && squares[r][c].isOccupied()) {
+			letters = letters + squares[r][c].getTile().getLetter();
+			if (mainWordIsHorizontal) {
+				r++;
+			} else {
+				c++;
+			}
+		}
+		return new Word (firstRow, firstCol, !mainWordIsHorizontal, letters);
+	}
+
+	public ArrayList<Word> getAllWords(Word mainWord, int BOARD_SIZE, Square[][] squares) {
+		ArrayList<Word> words = new ArrayList<>();
+		words.add(mainWord);
+		int r = mainWord.getFirstRow();
+		int c = mainWord.getFirstColumn();
+		for (int i=0; i<mainWord.length(); i++) {
+			//if (newLetterCoords.contains(new Coordinates(r,c))) {
+				if (isAdditionalWord(r, c, mainWord.isHorizontal(), BOARD_SIZE, squares)) {
+					words.add(getAdditionalWord(r, c, mainWord.isHorizontal(), BOARD_SIZE, squares));
+				//}
+			}
+			if (mainWord.isHorizontal()) {
+				c++;
+			} else {
+				r++;
+			}
+		}
+		return words;
+	}*/
 }
 
